03dd297bd0ee491b4651ba02a0519672e49cc398
SimonaDiFiandra <113619164+SimonaDiFiandra@users.noreply.github.com>
2022-11-15 14:12:07 +0100

    Initial commit

---
+++ Contapassi/ContapassiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContapassiApp.swift
+//  Contapassi
+//
+//  Created by Simona Di Fiandra on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ContapassiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
